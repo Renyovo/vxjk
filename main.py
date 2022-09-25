35ebde463cf2bcfdf7aebc78b6b1111a28cc0fc4
Renyovo <93809885+Renyovo@users.noreply.github.com>
2022-09-25 13:36:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,11 +21,7 @@ template_id = os.environ["TEMPLATE_ID"]
 
 def get_weather():
   url = "http://wthrcdn.etouch.cn/weather_mini?city=" + city
-  try:
-    res = requests.get(url, verify=False).json()
-  except:
-    res = requests.get(url, verify=False).text
-  print(res)
+  res = requests.get(url, verify=False).json()
   weather = res["data"]["forecast"][0]["type"]
   high = res["data"]["forecast"][0]["high"].replace("高温 ","")
   low = res["data"]["forecast"][0]["low"].replace("低温 ","")

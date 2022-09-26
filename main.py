4ca913852b287a51687b5f29933067c1894fba69
Renyovo <93809885+Renyovo@users.noreply.github.com>
2022-09-26 10:42:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
+birthday2 = os.environ['BIRTHDAY2']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
@@ -20,15 +21,19 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "http://wthrcdn.etouch.cn/weather_mini?city=" + city
-  res = requests.get(url, verify=False).json()
+  url = "http://t.weather.sojson.com/api/weather/city/" + city
+  res = requests.get(url).json()
   weather = res["data"]["forecast"][0]["type"]
   high = res["data"]["forecast"][0]["high"].replace("高温 ","")
   low = res["data"]["forecast"][0]["low"].replace("低温 ","")
-  fx = res["data"]["forecast"][0]["fengxiang"] + re.findall("\d+级",res["data"]["forecast"][0]["fengli"])[0]
+  fx = res["data"]["forecast"][0]["fx"] + res["data"]["forecast"][0]["fl"]
   tem = res["data"]["wendu"] + "℃"
-  ganmao = res["data"]["ganmao"]
-  return weather,high,low,fx,tem,ganmao
+  notice = res["data"]["forecast"][0]["notice"]
+  quality = res['data']['quality']
+  sunrise = res["data"]["forecast"][0]["sunrise"]
+  sunset = res["data"]["forecast"][0]["sunset"]
+  loca = res["cityInfo"]["city"]
+  return weather,high,low,fx,tem,notice,quality,sunrise,sunset,loca
 
 def get_day():
   week_list = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
@@ -44,6 +49,13 @@ def get_birthday():
     next = next.replace(year=next.year + 1)
   return (next - today).days
 
+def get_birthday2():
+  next = datetime.strptime(str(date.today().year) + "-" + birthday2, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
@@ -57,12 +69,14 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, high, low, fx, temperature, ganmao = get_weather()
-data = {"data":{"value":get_day(), "color":get_random_color()},"city":{"value":city, "color":get_random_color()},
-        "weather":{"value":wea, "color":get_random_color()},"high":{"value":high, "color":get_random_color()},
-        "low":{"value":low, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},
-        "wind":{"value":fx, "color":get_random_color()},"ganmao":{"value":ganmao, "color":get_random_color()},
+weather,high,low,fx,tem,notice,quality,sunrise,sunset,loca = get_weather()
+data = {"data":{"value":get_day(), "color":get_random_color()},"city":{"value":loca, "color":get_random_color()},
+        "weather":{"value":weather, "color":get_random_color()},"high":{"value":high, "color":get_random_color()},
+        "low":{"value":low, "color":get_random_color()},"temperature":{"value":tem, "color":get_random_color()},
+        "wind":{"value":fx, "color":get_random_color()},"notice":{"value":notice, "color":get_random_color()},
+        "sunrise":{"value":sunrise, "color":get_random_color()},"sunset":{"value":sunset, "color":get_random_color()},
         "love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},
-        "words":{"value":get_words(), "color":get_random_color()}}
+        "words":{"value":get_words(), "color":get_random_color()},"quality":{"value":quality, "color":get_random_color()},
+        "birthday_left2":{"value":get_birthday2(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
